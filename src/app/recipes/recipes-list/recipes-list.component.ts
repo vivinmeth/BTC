31b23b44be247aa22d6fc455a14dd7c42031da82
VivinMeth <vivinmeth.lv@emplay.net>
2020-01-11 23:27:56 +0530

    Adding content to the Recipes Component and Output the list of recipes with ngFor

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe("Hot Water","A Simple Recipe to Make Hot Water","https://www.rd.com/wp-content/uploads/2017/09/01_hotwater_Surprising-Benefits-of-Drinking-Hot-Water_10126957-Baevskiy-Dmitry-760x506.jpg")
+  ];
   constructor() { }
 
   ngOnInit() {
